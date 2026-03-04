dd78e0e075bcbb683c46b864f8c695271c65fd8d
fannybillefalt <fannybillefalt@gmail.com>
2026-03-04 16:13:49 +0100

    fyllt i cv.js

---
+++ cv.js
@@ -0,0 +1,43 @@
+async function loadCV() {
+  try {
+    const response = await fetch("cv.json");
+    const data = await response.json();
+
+    const workSection = document.getElementById("work-section");
+
+    workSection.innerHTML = '';
+
+    data.work.forEach((job) => {
+      const article = document.createElement("article");
+      article.className = "cv-item";
+
+      article.innerHTML = `
+        <h4>${job.company}</h4>
+        <h5>${job.role} | ${job.period}</h5>
+        ${job.description ? `<p>${job.description}</p>` : ""}`;//ternary, om truthy skriv ut beskrivning, falsy en tom sträng
+
+      workSection.appendChild(article);
+    });
+
+    const educationSection = document.getElementById("education-section");
+    educationSection.innerHTML = '';//tömmer först för att förhindra duplicering
+
+    data.education.forEach((education) => {
+      const article = document.createElement("article");
+      article.className = "cv-item";
+
+      article.innerHTML = `
+    <h4>${education.school}</h4>
+    <h5>${education.degree} | ${education.period}</h5>
+    ${education.note ? `<p>${education.note}</p>` : ""}`;
+
+      educationSection.appendChild(article);
+    });
+  } catch (error) {
+    console.log("Misslyckades att ladda CV:", error);
+  }
+}
+
+
+
+loadCV();
